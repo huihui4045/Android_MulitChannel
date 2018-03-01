78e2af9bd73548b1feaae71ad807002c557931bc
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2018-03-01 14:27:26 +0800

    dev分支

--- app/src/main/java/com/alizhezi/mulitchannel/MainActivity.java
+++ app/src/main/java/com/alizhezi/mulitchannel/MainActivity.java
@@ -1,10 +1,13 @@
 package com.alizhezi.mulitchannel;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,45 @@ public class MainActivity extends AppCompatActivity {
 
         textView.setText(BuildConfig.ENVIRONMENT);
 
-        textView.setOnClickListener(new View.OnClickListener() {
+        /*textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
                 startActivity(new Intent(MainActivity.this,QQActivity.class));
             }
-        });
+        });*/
+
+
+        Toast.makeText(this,getAppMetaData(this,"CHANNEL"),Toast.LENGTH_LONG).show();
+    }
+
+
+    /**
+     * 获取渠道名字
+     * 获取application中指定的meta-data。本例中，调用方法时key就是APP_CHANNEL
+     * @return 如果没有获取成功(没有对应值，或者异常)，则返回值为空
+     */
+    public static String getAppMetaData(Context ctx, String key) {
+        if (ctx == null || TextUtils.isEmpty(key)) {
+            return null;
+        }
+        String resultData = null;
+        try {
+            PackageManager packageManager = ctx.getPackageManager();
+            if (packageManager != null) {
+                ApplicationInfo applicationInfo = packageManager.getApplicationInfo(ctx.getPackageName(), PackageManager.GET_META_DATA);
+                if (applicationInfo != null) {
+                    if (applicationInfo.metaData != null) {
+                        resultData = applicationInfo.metaData.getString(key);
+                    }
+                }
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return resultData;
     }
 }
